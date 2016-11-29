b3a0819ae4e456cfd230518da281ccc0a28cf909
Luke Schlangen <lukeschlangen@gmail.com>
2016-11-29 10:35:11 -0600

    Added comments

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,12 +8,12 @@ app.controller('firstController', ['$http', function($http) {
   self.animal = {};
 
   self.getRandomPet = function(){
-    var query = 'http://api.petfinder.com/';
-    query += 'pet.getRandom';
-    query += '?key=' + key;
-    query += '&format=json';
-    query += '&output=basic';
-    var request = encodeURI(query) + '&callback=JSON_CALLBACK';
+    var query = 'http://api.petfinder.com/'; // baseURL for API
+    query += 'pet.getRandom'; // selecting the method we would like to return
+    query += '?key=' + key; // Giving petfinder our key
+    query += '&format=json'; // Telling petfinder we want our response to be JSON
+    query += '&output=basic'; // Telling petfinder what data we want (more than just id)
+    var request = encodeURI(query) + '&callback=JSON_CALLBACK'; // removing spaces and special characters from response as well as making jsonp work with the callback
 
     console.log('Request:', request);
 
